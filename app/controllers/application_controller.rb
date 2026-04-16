b04ea06b8967a83bc6aa258e6c54837eb3645afd
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-17 01:27:40 +0900

    ログインルーティング設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
   def configure_permitted_parameters
     # サインアップ時に name カラムの保存を許可する
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
+    home_path
+  end
+
+  def after_sign_in_path_for(resource)
     home_path
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
   def top
-    # ログイン画面用
+    # ログイン済みなら home へリダイレクト
+    redirect_to home_path if user_signed_in?
   end
 
   def signup
