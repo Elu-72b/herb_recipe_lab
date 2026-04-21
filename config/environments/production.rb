57a44d800b216e3893f8eaf41c206807589c57fb
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-21 23:50:32 +0900

    fix:デプロイエラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.active_storage.service = :cloudinary
+  #config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
 end
