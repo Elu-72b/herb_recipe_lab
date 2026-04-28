cc207c7c6e2cbe4f83ae6c2e716ff6ac6b888813
Elu-72b <eriloach0339vialattea@gmail.com>
2026-04-28 22:45:06 +0900

    feat: ハーブ配合をrenderできるよう実装

--- app/models/recipe_herb.rb
+++ app/models/recipe_herb.rb
@@ -1,11 +1,12 @@
 class RecipeHerb < ApplicationRecord
   belongs_to :recipe
-  belongs_to :herb
+  belongs_to :herb, optional: true
 
   # seeds.rbのunit設計に合わせた定義
   enum :unit, { g: 0, teaspoon: 1, piece: 2, individual: 3 }
 
   validates :quantity, presence: true,
     numericality: { greater_than: 0, message: "配合量は0より大きい値を入力してください" }
-  validates :herb_id, presence: { message: "ハーブを選択してください" }
+  validates :herb_id, presence: { message: "ハーブを選択してください" }, if: -> { custom_herb_name.blank? }
+  validates :custom_herb_name, presence: { message: "ハーブ名を入力してください" }, if: -> { herb_id.nil? }
 end
